4f024216d2f234e6f76f7990f31d2aee0f8e03b7
hasanalam24 <hasanalam365@gmail.com>
2024-05-06 16:03:34 +0600

    if user can do change his token name or invalid then he auto go to login page

--- src/BaseURL/UseAxiosSecure.jsx
+++ src/BaseURL/UseAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import UseAuthHooks from "../Hooks/UseAuthHooks";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000',
@@ -6,6 +9,27 @@ const axiosSecure = axios.create({
 })
 
 const UseAxiosSecure = () => {
+
+    const { logOut } = UseAuthHooks()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(res => {
+            return res;
+        }, error => {
+            console.log('error tracked', error.message)
+            if (error.message.status === 401 || error.message.status === 403) {
+                console.log('logOut')
+                logOut()
+                    .then(() => {
+                        navigate('/login')
+                    })
+                    .catch(error => console.log(error))
+            }
+        }
+        )
+    }, [])
+
     return axiosSecure;
 };
 
